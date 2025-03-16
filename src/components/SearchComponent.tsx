ece0b70e932de216ae3dacc651b7fcf4311188ad
sirhmvfx <sirhmvfx@gmail.com>
2025-03-16 15:32:06 +0100

    created reusable search component

---
+++ src/components/SearchComponent.tsx
@@ -0,0 +1,7 @@
+const SearchComponent = () => {
+    return (
+        <input type="text" className="p-2 outline-none text-xs border border-white/20 rounded-xl w-[400px]" placeholder="Search" />
+    )
+}
+
+export default SearchComponent
