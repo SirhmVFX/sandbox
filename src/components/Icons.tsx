0ef6b783530d722e612bdace6b0866030cd21501
sirhmvfx <sirhmvfx@gmail.com>
2025-03-21 15:59:32 +0100

    added search icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -392,3 +392,15 @@ export const UpVoteIcon = ({
     />
   </svg>
 );
+
+
+export const FileSearch = ({
+    size,
+    className,
+}: {
+    size: number;
+    className?: string;
+}) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={`lucide lucide-search-icon lucide-search ${className}`}><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+);
+    
