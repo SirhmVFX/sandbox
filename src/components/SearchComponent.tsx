b189f0589ba9b6eeff5df2f6093016a8c5c1561f
sirhmvfx <sirhmvfx@gmail.com>
2025-03-21 15:59:20 +0100

    implemented search modal ui

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -1,6 +1,25 @@
+"use client"
+import { useState } from "react"
+import { FileSearch } from "./Icons"
+
 const SearchComponent = () => {
+    const [searchDialog, setSearchDialog] = useState(false)
     return (
-        <input type="text" className="p-2 outline-none text-xs border border-white/20 rounded-xl w-[400px]" placeholder="Search" />
+        <>
+        {searchDialog && (
+            <div className="absolute p-20 left-0 right-0 top-0 bottom-0 z-10 bg-black/20 backdrop-blur-md flex flex-col items-center ">
+                <div  className=" flex items-center gap-2 p-6 outline-none text-xs border border-white/10 rounded-full w-[800px]">
+            <FileSearch size={17}/>
+            <input  type="text" className=" outline-none text-xs " placeholder="Search" />
+        </div>
+            </div>
+        )}
+       <div onClick={() => setSearchDialog(!searchDialog)}  className="flex items-center gap-2 p-2 outline-none text-xs border border-white/10 rounded-full w-[400px]">
+            <FileSearch size={17}/>
+            <input  type="text" className=" outline-none text-xs " placeholder="Search" />
+        </div>
+        </>
+        
     )
 }
 
