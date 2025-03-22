f12ebb7647d4eff95c3637bd9ef5988210c8b7e0
sirhmvfx <sirhmvfx@gmail.com>
2025-03-22 16:04:21 +0100

    added cacel icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -404,3 +404,13 @@ export const FileSearch = ({
   <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={`lucide lucide-search-icon lucide-search ${className}`}><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
 );
     
+
+export const Cancel = ({
+  size,
+  className,
+}: {
+  size: number;
+  className?: string;
+}) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={`lucide lucide-x-icon lucide-x ${className}`}><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+)
