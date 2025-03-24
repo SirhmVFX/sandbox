ab06425a6fd5cca23c7d03b4574f49f863ae924e
sirhmvfx <sirhmvfx@gmail.com>
2025-03-24 03:26:09 +0100

    updated sidebar comonent

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -8,7 +8,7 @@ const SearchComponent = () => {
         <>
         {searchDialog && (
             <div onClick={() => setSearchDialog(false)} className="absolute p-20 left-0 right-0 top-0 bottom-0 z-10 bg-black/20 backdrop-blur-md flex flex-col items-center ">
-                <div className="absolute top-10 right-10">
+                <div className=" cursor-pointer absolute top-10 right-10">
                     <Cancel size={20}/>
                 </div>
                 <div  className=" flex items-center gap-2 p-6 outline-none text-xs border border-white/10 rounded-full w-[800px]">
@@ -17,7 +17,7 @@ const SearchComponent = () => {
         </div>
             </div>
         )}
-       <div onClick={() => setSearchDialog(!searchDialog)}  className="flex items-center gap-2 p-2 outline-none text-xs border border-white/10 rounded-full w-[400px]">
+       <div onClick={() => setSearchDialog(!searchDialog)}  className="cursor-pointer flex items-center gap-2 p-2 outline-none text-xs border border-white/10 rounded-full w-[400px]">
             <FileSearch size={17}/>
             <input  type="text" className=" outline-none text-xs " placeholder="Search" />
         </div>
