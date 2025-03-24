152f1e9df549a07e3f3d31dfec7ff423aeef0bad
sirhmvfx <sirhmvfx@gmail.com>
2025-03-24 03:25:54 +0100

    updated posts card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,11 +2,12 @@
 
 import { useState } from "react";
 import { BookmarkIcon, CommentIcon, ShareIcon, StarIcon, UpVoteIcon } from "./Icons";
+import Link from "next/link";
 
 const PostCard = () => {
     const [showEmojis, setShowEmojis] = useState(false)
     return (
- <div className="py-8 flex flex-col gap-2 border-b border-white/5">
+ <Link href={"1"} className="py-8 flex flex-col gap-2 border-b border-white/5">
        <h1 className="text-sm">
          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
          numquam recusandae eligendi voluptate sequi illum reiciendis accusamus
@@ -30,12 +31,17 @@ const PostCard = () => {
             }
          <button className="flex gap-1 items-center transition-all" onClick={() => setShowEmojis(!showEmojis)}>
            {" "}
-           <p className="text-[12px] ">🔥⚡️👍🏼❤️</p>{" "}
-           <p className="text-[12px] text-primary-color/50"> 20k</p>
+           <p className="text-[12px] ">🔥⚡️❤️</p>{" "}
+           <p className="text-[12px] text-white/10"> 20k</p>
          </button>
          <button className="flex gap-1 items-center">
            <CommentIcon size={12} className="text-primary-color" />
-           <p className="text-[12px] text-primary-color/50">24k</p>
+           <p className="text-[12px] text-white/10">24k</p>
+         </button>
+
+         <button className="flex gap-1 items-center">
+           <CommentIcon size={12} className="text-white/10" />
+           <p className="text-[12px] text-white/10">2290M</p>
          </button>
  
          <button className="flex items-center gap-1">
@@ -52,7 +58,7 @@ const PostCard = () => {
            </button>
          </div>
        </div>
-     </div>       
+     </Link>       
     )
 }
 
